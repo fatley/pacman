82e1b4681a43e80a164f71ffb2a6190b2d517e7f
Ashley Pham <68831911+fatley@users.noreply.github.com>
2023-11-03 11:42:45 -0700

    mini max 4/5!

--- pacai/student/multiagents.py
+++ pacai/student/multiagents.py
@@ -57,109 +57,75 @@ class ReflexAgent(BaseAgent):
         # newScaredTimes = [ghostState.getScaredTimer() for ghostState in newGhostStates]
 
         # *** Your Code Here ***
-        # newPosition = successorGameState.getPacmanPosition()
-        # oldFood = currentGameState.getFood()
-        # newGhostStates = successorGameState.getGhostStates()
-        # newScaredTimes = [ghostState.getScaredTimer() for ghostState in newGhostStates]
-        
-        # # checking for each ghost
-        # # grabbing curr position of pacman & ghost
-        # oldPosition = currentGameState.getPacmanPosition()
-        # oldGhostStates = currentGameState.getGhostStates()
-        # # for each ghost, check if the new position is closer than the old position
-        # for oldGhostState, newGhostState in zip(oldGhostStates, newGhostStates):
-        #     oldGhostPosition = oldGhostState.getPosition()
-        #     newGhostPosition = newGhostState.getPosition()
-            
-        #     # if the new position is closer than the old position, then return -inf
-        #     oldDistance = manhattan(oldPosition, oldGhostPosition)
-        #     newDistance = manhattan(newPosition, newGhostPosition)
-            
-        #     if newDistance < 2 and newDistance < oldDistance:
-        #         return float("-inf")
-        # # capsules
-        # capsules = currentGameState.getCapsules()
-        # if capsules:
-        #     closestCapsule = min(capsules, key=lambda capsule: manhattan(newPosition, capsule))
-        #     for ghostState in newGhostStates:
-        #         if manhattan(ghostState.getPosition(), closestCapsule) < 2:
-        #             foodList = oldFood.asList()
-        #             if foodList:
-        #                 closestFood = min([manhattan(newPosition, food) for food in foodList])
-        #                 if closestFood == 0:
-        #                     return successorGameState.getScore() + 1
-        #                 else:
-        #                     return successorGameState.getScore() + 1 / closestFood
-        
-        # # if the ghost is scared, eat everythign in sight while the ghost is scared
-        # if newScaredTimes[0] > 0:
-        #     foodList = oldFood.asList()
-        #     if foodList:
-        #         closestFood = min([manhattan(newPosition, food) for food in foodList])
-        #         if closestFood == 0:
-        #             return successorGameState.getScore() + 1
-        #         else:
-        #             return successorGameState.getScore() + 1 / closestFood
-        #     else:
-        #         return successorGameState.getScore() + 1000
-        
-        # # if there are no capsules, carry on with normal routine
-        # # checking for food
-        # foodList = oldFood.asList()
-        # if foodList:
-        #     closestFood = min([manhattan(newPosition, food) for food in foodList])
-        #     if closestFood == 0:
-        #         return successorGameState.getScore() + 1
-        #     else:
-        #         return successorGameState.getScore() + 1 / closestFood
-    
-        # # checking for ghost
-        # for ghostState in newGhostStates:
-        #     if ghostState.getPosition() < 2:
-        #         return float("-inf")
-            
-        # # if there are no ghost, eat all teh food around
-        # if not any(ghostState.getPosition() < 3 for ghostState in newGhostStates):
-        #     return successorGameState.getScore() + 1000
-        
-        # return successorGameState.getScore()
-    
-        successorGameState = currentGameState.generatePacmanSuccessor(action)
         newPosition = successorGameState.getPacmanPosition()
         oldFood = currentGameState.getFood()
         newGhostStates = successorGameState.getGhostStates()
         newScaredTimes = [ghostState.getScaredTimer() for ghostState in newGhostStates]
+        
 
-        # walls
-        walls = currentGameState.getWalls()
-        x, y = newPosition
-        if sum([walls[x+dx][y+dy] for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]]) >= 3:
-            return float("-inf")
-
-        # Calculate the distance to the closest ghost
-        ghostDistances = [manhattan(newPosition, ghostState.getPosition()) for ghostState in newGhostStates]
-        closestGhostDistance = min(ghostDistances) if ghostDistances else float("inf")
-
-        # Calculate the distance to the closest food
-        foodDistances = [manhattan(newPosition, food) for food in oldFood.asList()]
-        closestFoodDistance = min(foodDistances) if foodDistances else float("inf")
-
-        # Calculate the score
-        score = successorGameState.getScore()
-
-        # If there's a ghost too close, return a very low score
-        if closestGhostDistance < 2:
-            return float("-inf")
-
-        # If there's a scared ghost, prioritize eating food
+        # checking for each ghost
+        # grabbing curr position of pacman & ghost
+        oldPosition = currentGameState.getPacmanPosition()
+        oldGhostStates = currentGameState.getGhostStates()
+        # for each ghost, check if the new position is closer than the old position
+        for oldGhostState, newGhostState in zip(oldGhostStates, newGhostStates):
+            oldGhostPosition = oldGhostState.getPosition()
+            newGhostPosition = newGhostState.getPosition()
+            
+            # if the new position is closer than the old position, then return -inf
+            oldDistance = manhattan(oldPosition, oldGhostPosition)
+            newDistance = manhattan(newPosition, newGhostPosition)
+            
+            if newDistance < 2 and newDistance < oldDistance:
+                return float("-inf")
+        # capsules
+        capsules = currentGameState.getCapsules()
+        if capsules:
+            closestCapsule = min(capsules, key=lambda capsule: manhattan(newPosition, capsule))
+            for ghostState in newGhostStates:
+                if manhattan(ghostState.getPosition(), closestCapsule) < 2:
+                    foodList = oldFood.asList()
+                    if foodList:
+                        closestFood = min([manhattan(newPosition, food) for food in foodList])
+                        if closestFood == 0:
+                            return successorGameState.getScore() + 1
+                        else:
+                            return successorGameState.getScore() + 1 / closestFood
+        
+        # if the ghost is scared, eat everythign in sight while the ghost is scared
         if newScaredTimes[0] > 0:
-            return score + 1 / closestFoodDistance if closestFoodDistance != 0 else score + 1
-
-        # If there are no ghosts too close, prioritize eating food
-        if not any(manhattan(newPosition, ghostState.getPosition()) < 3 for ghostState in newGhostStates):
-            return score + 1 / closestFoodDistance if closestFoodDistance != 0 else score + 1
-
-        return score   
+            foodList = oldFood.asList()
+            if foodList:
+                closestFood = min([manhattan(newPosition, food) for food in foodList])
+                if closestFood == 0:
+                    return successorGameState.getScore() + 1
+                else:
+                    return successorGameState.getScore() + 1 / closestFood
+            else:
+                return successorGameState.getScore() + 1000
+        
+        # if there are no capsules, carry on with normal routine
+        # checking for food
+        foodList = oldFood.asList()
+        if foodList:
+            closestFood = min([manhattan(newPosition, food) for food in foodList])
+            if closestFood == 0:
+                return successorGameState.getScore() + 1
+            else:
+                return successorGameState.getScore() + 1 / closestFood
+    
+        # checking for ghost
+        for ghostState in newGhostStates:
+            if ghostState.getPosition() < 2:
+                return float("-inf")
+            
+        # if there are no ghost, eat all teh food around
+        if not any(ghostState.getPosition() < 3 for ghostState in newGhostStates):
+            return successorGameState.getScore() + 1000
+        
+        return successorGameState.getScore()
+    
+    
     
     
 class MinimaxAgent(MultiAgentSearchAgent):
@@ -206,83 +172,40 @@ class MinimaxAgent(MultiAgentSearchAgent):
         depth = self.getTreeDepth()
         return self.minimax(gameState, depth, 0)[1]
         
-    # minimax func
-    # returns the minimax value of a state
+    # psuedocode found on - https://www.youtube.com/watch?v=l-hh51ncgDI
+    # minimax function (positiionn, depth, maximizing player)
     def minimax(self, state, depth, agentIndex):
-        # if state is terminal, return the state's utility
-        if state.isWin() or state.isLose() or depth == 0:
-            # return state.evaluationFunction(state), None
-            return self.getEvaluationFunction()(state), None
+        # if depth == 0 or game over in position, return static eval of pos
+        if depth == 0 or state.isWin() or state.isLose():
+            return self._evaluationFunction(state), None
         
-        # if agent is pacman, return max value
+        # if maximizing player (pacman)
         if agentIndex == 0:
-            return self.maxValue(state, depth, agentIndex)
-        
-        # if agent is ghost, return min value
+            # max evaluation = -inf
+            maxEval = float("-inf")
+            # for each child of position, eval = minimax(child, depth - 1, false)
+            for action in state.getLegalActions(agentIndex):
+                # eval = minimax( child, depth -1, false)
+                eval = self.minimax(state.generateSuccessor(agentIndex, action), depth - 1, 1)[0]
+                # comparing curent action (eval) w / highest action (maxeval) 
+                if eval > maxEval:
+                    maxEval = max(maxEval, eval)
+                    maxAction = action
+            return maxEval, maxAction
+            
         else:
-            return self.minValue(state, depth, agentIndex)
-        
-    # max value func
-    # returns the max value of a state
-    def maxValue(self, state, depth, agentIndex):
-        # if state is terminal, return the state's utility
-        if state.isWin() or state.isLose():
-            return state.getScore(), None
-        
-        # set v to -inf
-        v = float("-inf")
-        # set action to None
-        maxAction = None
-        
-        # # for each action in legal actions
-        # for action in state.getLegalActions(agentIndex):
-        #     # set v to max of v and min value of successor state
-        #     v = max(v, self.minimax(state.generateSuccessor(agentIndex, action), depth, agentIndex + 1)[0])
-        
-        # # return v and action
-        # return v, action
-        
-        for action in state.getLegalActions(agentIndex):
-            newValue = self.minimax(state.generateSuccessor(agentIndex, action), depth, agentIndex + 1)[0]
-            if newValue > v:
-                v, maxAction = newValue, action
-                
-        return v, maxAction
+            # minEval = +inf
+            minEval = float("inf")
+            # for each child of position, eval = minimax(child, depth - 1, true)
+            # minieval = min(mineval, eval)
+            for action in state.getLegalActions(agentIndex):
+                eval = self.minimax(state.generateSuccessor(agentIndex, action), depth - 1, 0)[0]
+                # comparing current action w/ lowest
+                if eval < minEval:
+                    minEval = min(minEval, eval)
+                    minAction = action
+            return minEval, minAction
     
-    def minValue(self, state, depth, agentIndex):
-        # if state is terminal, return the state's utility
-        if state.isWin() or state.isLose():
-            return state.getScore(), None
-        
-        # set v to inf
-        v = float("inf")
-        # set action to None
-        minAction = None
-        
-        # for each action in legal actions
-        # for action in state.getLegalActions(agentIndex):
-        #     # if agent is the last ghost, set v to min of v and max value of successor state
-        #     if agentIndex == state.getNumAgents() - 1:
-        #         v = min(v, self.minimax(state.generateSuccessor(agentIndex, action), depth - 1, 0)[0])
-        #     # else, set v to min of v and min value of successor state
-        #     else:
-        #         v = min(v, self.minimax(state.generateSuccessor(agentIndex, action), depth, agentIndex + 1)[0])
-        
-        # # return v and action
-        # return v, action
-        
-        for action in state.getLegalActions(agentIndex):
-            # if agent is the last ghost, move to the next depth level and reset agentIndex to 0
-            if agentIndex == state.getNumAgents() - 1:
-                newValue = self.minimax(state.generateSuccessor(agentIndex, action), depth - 1, 0)[0]
-            # else, continue with the next ghost
-            else:
-                newValue = self.minimax(state.generateSuccessor(agentIndex, action), depth, agentIndex + 1)[0]
-
-            if newValue < v:
-                v, minAction = newValue, action
-
-        return v, minAction
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
